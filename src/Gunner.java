8885d8bdfaa27aaf7587a6306a5f8d2de243978e
Sallador <sallador87@yahoo.com>
2015-08-22 13:57:49 +0700

    an initial test commit

---
+++ src/Gunner.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Gunner {
+    private final String name = "Marthen";
+    private final String surname = "Fourcade";
+    private final Date birthDate = new Date();
+
+    public static void main(String[] args) {
+        System.out.println(new Gunner());
+    }
+}
+
