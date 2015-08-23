2f697bca00be7e76e951f8cc83076ee6830a5e8d
Sallador <sallador87@yahoo.com>
2015-08-23 16:11:32 +0700

    update

--- src/Gunner.java
+++ src/Gunner.java
@@ -1,12 +1,22 @@
+import java.lang.reflect.Array;
 import java.util.Date;
 
 public class Gunner {
     private final String name = "Marthen";
     private final String surname = "Fourcade";
     private final Date birthDate = new Date();
+    static void test(String arg1, String... args) {
 
+    }
     public static void main(String[] args) {
+
         System.out.println(new Gunner());
+        int[] arr = new int[] {1, 2, 3, 4, 6, 77};
+        System.out.println(Integer.MIN_VALUE - 1);
+        int[] arr2 [][]  = new int[4][4][2];
+
+
+
     }
 }
 
